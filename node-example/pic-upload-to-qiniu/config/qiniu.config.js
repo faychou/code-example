ea38274e537a64d36fb0828781de6d5f19f8dc22
faychou <fay_chou@163.com>
2020-05-18 18:20:08 +0800

    'commit'

--- node-example/pic-upload-to-qiniu/config/qiniu.config.js
+++ node-example/pic-upload-to-qiniu/config/qiniu.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  accessKey: 'WT7MbQ2De_9Towks9mOWtnf28XBw9nDKf1uboi5n', // accessKey
-  secretKey: 'HYPuWzdm_9dyQVelzNgv6aEUff20S9_BK7AJUsGg', // secretKey
-  bucket: 'fcs-public', // 要上传的空间名(仓库名字)
+  accessKey: '', // accessKey
+  secretKey: '', // secretKey
+  bucket: '', // 要上传的空间名(仓库名字)
   qiniuUrl: 'q9xv20b60.bkt.clouddn.com' // 空间绑定的域名
 }
